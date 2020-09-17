fe6f7c2021ec2dc5c9390b6991dd248543ee835f
Edward Atencio <ed.atencio16@gmail.com>
2020-09-17 11:01:51 -0500

    build

---
+++ precache-manifest.00d04a699aef686994ca2b1169843781.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcff56140d4879d6897851c9a81cfc02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0adf53093ae79b2d7f07",
+    "url": "/static/js/2.266612ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.266612ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d36d4e8748656df16cbc",
+    "url": "/static/js/main.53f0780e.chunk.js"
+  },
+  {
+    "revision": "f63fb8479a2bf060892a",
+    "url": "/static/js/runtime-main.dca36505.js"
+  }
+]);
