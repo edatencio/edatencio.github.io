87ffc7d61e4c68e74e099b54fce11c68441236e0
Edward Atencio <ed.atencio16@gmail.com>
2020-09-17 10:31:42 -0500

    portfolio items show gallery or external link

--- src/src/components/PortfolioItem.js
+++ src/src/components/PortfolioItem.js
@@ -4,20 +4,32 @@ import PortfolioModal from "./PortfolioModal";
 const PortfolioItem = ({ item }) =>
 {
     const modalId = "portfolioModal" + item.id;
+    const iconClass = "fas " + (item.externalLink ? "fa-external-link-alt" : "fa-search") + " fa-2x";
+
+    const Link = (props) =>
+    {
+        if (item.externalLink)
+            return <a className="portfolio-item mx-auto" href={item.externalLink} target="_blank">{props.children}</a>;
+        else
+            return <div className="portfolio-item mx-auto" data-toggle="modal" data-target={"#" + modalId}>{props.children}</div>;
+    }
 
     return (
         <div className={"col-md-6 col-lg-4 mb-5 filter " + item.filter}>
-            <div className="portfolio-item mx-auto" data-toggle="modal" data-target={"#" + modalId}>
+            <Link>
                 <div className="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100 bg-gradient">
-                    <div className="portfolio-item-caption-content text-center text-white"><i className="fas fa-search fa-2x"></i></div>
+                    <div className="portfolio-item-caption-content text-center text-white">
+                        <i className={iconClass}></i>
+                    </div>
                 </div>
                 <img className="img-fluid" src={item.bannerURL} alt="" />
-            </div>
+            </Link>
             <div className="mt-3">
                 <h5 className="text-center">{item.title}</h5>
                 <p className="text-center text-muted">{item.filter}</p>
             </div>
-            <PortfolioModal item={item} id={modalId} />
+
+            {item.images ? <PortfolioModal item={item} id={modalId} /> : null}
         </div>
     );
 };
